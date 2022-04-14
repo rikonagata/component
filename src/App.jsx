7832a0f8e173a92f7aa8adb905fb9b18eb0800d2
rikonagata <103544713+rikonagata@users.noreply.github.com>
2022-04-14 17:26:28 +0900

    0414

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 //Reactのコンポーネント専用のファイルを拡張子jsxで記述するとReact使ってることが分かりやすい
-
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  //　↓1つ目がstjavascriptの要素の中に直接オブジェクトとしてcssを充てるか　変数を宣言して、コンポーネントないで呼出すか　cssのファイルをインポートするかateの変数名、2つ目がstateを更新する関数名
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  //　↓1つ目がstjavascriptの要素の中に直接オブジェクトとしてcssを充てるか　変数を宣言して、コンポーネントないで呼出すか　cssのファイルをインポートするかateの変数名、2つ目がstateを更新する関数名
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+  }, [num]);
+
   //{}
   //<ColorfulMessage>でコンポーネントタグ指定して、その中にpropsとして渡すものを書く
   return (
@@ -17,8 +31,10 @@ const App = () => {
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
-      <p>(^^♪</p>
+      {faceShowFlag && <p>(^^♪</p>}
     </>
   );
 };
